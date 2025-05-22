189399b6a2eeeddfd4139ccbbc769a6df737d0bd
ewllewis <ewl.lewis@gmail.com>
2025-05-22 10:35:24 +0100

    add comments to single article page

--- src/components/ArticleGallery.jsx
+++ src/components/ArticleGallery.jsx
@@ -1,11 +1,14 @@
 import ArticleGalleryCard from "./ArticleGalleryCard";
 import "../styles/articleGallery.css";
+import { Link } from "react-router";
 
 function ArticleGallery({ articles }) {
   return (
     <div className="article-gallery-container">
       {articles.map((article) => (
-        <ArticleGalleryCard key={article.article_id} article={article} />
+        <Link to={`/article/${article.article_id}`} key={article.article_id}>
+          <ArticleGalleryCard key={article.article_id} article={article} />
+        </Link>
       ))}
     </div>
   );
